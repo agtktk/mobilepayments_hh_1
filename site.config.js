56a6c6e10be44f9258490ce92d6474905c5e40f0
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 11:50:32 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ export const siteConfig = {
   siteDescription: '소액결제티켓 정식 매입 업체. 모바일 티켓 및 상품권 당일 정산, 24시간 친절 상담으로 안전 거래 보장.',
   author: '희망티켓 010-9469-1051',
   defaultImage: '/images/sample.png',
-  naverVerificationCode: '',
+  naverVerificationCode: '56b0ebfdda99f179a0703d60a4533e15bfbd8537',
   googleVerificationCode: '',
   company: '희망티켓',
   phone: '01094691051',
